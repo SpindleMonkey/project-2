944e1c131378efbf8142945f45b7f36983b8d42c
connie kephart <spindle.monkey@gmail.com>
2017-08-06 18:43:58 -0600

    adds unit tests

---
+++ test/breed_spec.js
@@ -0,0 +1,92 @@
+expect = require('chai').expect;
+request = require('request');
+
+describe('sheepList', function() {
+  let myApi;
+  let myErr;
+  let myResponse;
+  let myBody;
+
+  before(function(done) {
+    myApi = 'http://localhost:3000/api/breed/';
+    request(myApi, function(err, response, body) {
+      myErr = err;
+      console.log('err: ' + err);
+      myResponse = response;
+      if (typeof(body) === "string") {
+        myBody = JSON.parse(body);
+      } else {
+        myBody = body;
+      }
+      done();
+    });
+  });
+
+  it('should receive 200/ok HTTP status code', function() {
+    expect(myResponse.statusCode).to.eq(200);
+  });
+
+  it('should return an array', function() {
+    expect(myBody).to.be.an('array');
+  });
+
+  it('should have 30 items in the array (following a seed)', function(){
+    expect(myBody.length).to.be.eq(30);
+  });
+
+  it('should return \'Bluefaced Leicester\' for the first array element', function() {
+    expect(myBody[0]).to.eq('Bluefaced Leicester');
+  });
+
+  it('should return \'Wensleydale\' for the first array element', function() {
+    expect(myBody[myBody.length - 1]).to.eq('Wensleydale');
+  });
+
+});
+
+describe('sheepDetail', function() {
+  let myApi;
+  let myErr;
+  let myResponse;
+  let myBody;
+
+  before(function(done) {
+    myApi = 'http://localhost:3000/api/breed/Cormo';
+    request(myApi, function(err, response, body) {
+      myErr = err;
+      console.log('err: ' + err);
+      myResponse = response;
+      if (typeof(body) === "string") {
+        myBody = JSON.parse(body);
+      } else {
+        myBody = body;
+      }
+      done();
+    });
+  });
+
+  it('should receive 200/ok HTTP status code', function() {
+    expect(myResponse.statusCode).to.eq(200);
+  });
+
+  it('should return an object', function() {
+    expect(myBody).to.be.an('object');
+  });
+
+  it('should have 1 items in the array (following a seed)', function(){
+    expect(myBody.breeds.length).to.be.eq(1);
+  });
+
+  it('should return a name of \'Cormo\'', function() {
+    expect(myBody.breeds[0].name).to.eq('Cormo');
+  });
+
+  it('should return an origin of \'Australia\'', function() {
+    expect(myBody.breeds[0].origin).to.eq('Australia');
+  });
+
+  it('should have a description', function() {
+    expect(myBody.breeds[0].description).to.not.be.empty;
+  })
+
+});
